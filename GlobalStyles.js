0be63397829cfe2970d9053e8a735590dbfb287e
scorbac <jose.lopez17@tectijuana.edu.mx>
2018-08-15 16:20:58 -0700

    test

--- GlobalStyles.js
+++ GlobalStyles.js
@@ -7,6 +7,7 @@ export default  StyleSheet.create({
     textAlign: 'center',
     margin: 10,
   },
+    //Global
     FirstScreen:{
         flex: 1, 
         alignItems: 'center', 
@@ -39,30 +40,45 @@ export default  StyleSheet.create({
       color: '#ffffff',
       fontSize: 18,
     },
+    //ScreenA
     container_ScreenA: {
       flex: 1,
       justifyContent: 'center',
       alignItems: 'center',
       backgroundColor: '#D33232',
     },
+    //ScreenB
     container_ScreenB: {
       flex: 1,
       justifyContent: 'center',
       alignItems: 'center',
       backgroundColor: '#4D98EC',
     },
+
+
+
+
     container: {
       flex: 1,
-      alignItems: 'center',
+      //justifyContent: 'center',//centrar verticalmente
+      alignItems: 'center',//centrar horizontalmente
+      //backgroundColor: '#F5FCFF',
     },
     Font:{
       fontFamily: 'Roboto', 
       fontSize: 30
     },
     BgColor:{
-      backgroundColor: '#c5cae9'
+      backgroundColor: '#c5cae9'//"#bcddf2"
     },
     icon:{
-      color : '#01579b'
+      //color:'#013e89'
+      color : '#01579b'//light-blue darken-4
     }
   });
+
+  /*
+      icon: {
+      width: 24,
+      height: 24,
+    } */
